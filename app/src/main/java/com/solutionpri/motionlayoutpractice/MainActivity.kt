31c1a4bfd8c4e4cd4febfaaa83bef3bc09b9a710
Adnan Habib <28705562+AdnanHabibMirza@users.noreply.github.com>
2021-02-12 12:54:23 +0500

    Initial commit

---
+++ app/src/main/java/com/solutionpri/motionlayoutpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.solutionpri.motionlayoutpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
